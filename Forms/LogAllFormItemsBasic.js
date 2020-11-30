6e9830d54cd41bded801e03a614ec537a848aa82
PGSystemTester <7731989+PGSystemTester@users.noreply.github.com>
2020-11-30 10:55:29 -0800

    Update LogAllFormItemsBasic.js
    Improved Logging efficiency.

--- Forms/LogAllFormItemsBasic.js
+++ Forms/LogAllFormItemsBasic.js
@@ -1,17 +1,13 @@
 function LogAllFormItemsBasic() {
-  var theForm = FormApp.getActiveForm();
-  var allItems = theForm.getItems();
-  for (var i = 0; i < allItems.length; i++) {
-    var theItem = allItems[i];
-    Logger.log('\nThe Title: ' +
-      theItem.getTitle() + '\n' +
-      'Help Text: ' +
-      theItem.getHelpText() + '\n' +
-      'The ID: ' +
-      theItem.getId() + '\n' +
-      'The Type: ' +
-      theItem.getType() + '\n'
-    );
-  }
-  Logger.log("All Items Shown");
+  var thisForm = FormApp.getActiveForm();
+  var theText =  ''
+  thisForm.getItems().forEach(function(theItem){
+    theText = theText 
+      + '\n\nIndex: ' + theItem.getIndex()
+      + '\n\tID:' + theItem.getId()
+      + '\n\tTitle:' + theItem.getTitle()
+      + '\n\tHelpText:' + theItem.getHelpText()
+      + '\n\tType:' + theItem.getType()
+  });
+  Logger.log(theText);
 }
