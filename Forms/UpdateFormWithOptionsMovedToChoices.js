a7e2936795a83eba2ba2d693f3bb77ea210d5eac
PGSystemTester <7731989+PGSystemTester@users.noreply.github.com>
2021-09-12 18:48:27 -0700

    Update UpdateFormWithOptionsMovedToChoices.js

--- Forms/UpdateFormWithOptionsMovedToChoices.js
+++ Forms/UpdateFormWithOptionsMovedToChoices.js
@@ -3,9 +3,8 @@
  */
 
 /*
-This procedure will update any multiple 
-choice orCheckbox questions (Items) with 
-a user's submission in the "Other" text.
+These procedures will update all questions with "Other"
+choice or Checkbox questions (Items).
 */
 
 const thisForm = FormApp.getActiveForm();
@@ -13,33 +12,31 @@ const thisForm = FormApp.getActiveForm();
 /**
  * Executed when new entry received. This needs to be setup using EDIT --->  Current Project Triggers
  */
-function entryMade(e){
-
+function entryMade(e) {
   reviewResponse_(e.response);
 }
 
 /**
- * Simulates a submission -- only used for testing. Not necassary to run.
+ * Simulates a submission with most recent response. Only for testing.
  */
-function testEntryMade(){
+function testEntryMade() {
   var allResponses = thisForm.getResponses();
-  var aResponse = allResponses[allResponses.length-9];
-    reviewResponse_(aResponse);
+  var aResponse = allResponses[allResponses.length - 1];
+  reviewResponse_(aResponse);
 }
-                  
+
 /**
  * Takes a response to a form and reviews if any "other" options were checked.
  */
-function reviewResponse_(fullResponse){
-  
+function reviewResponse_(fullResponse) {
   var fullIndividualResponses = fullResponse.getItemResponses();
-  for(i=0;i<fullIndividualResponses.length;i++){
+  for (i = 0; i < fullIndividualResponses.length; i++) {
     var eachResponse = fullIndividualResponses[i];
     var theQuestion = eachResponse.getItem();
-    if(theQuestion.getType() == FormApp.ItemType.MULTIPLE_CHOICE){
-      checkAnswers_([eachResponse.getResponse()],theQuestion.asMultipleChoiceItem())
-    } else if(theQuestion.getType() == FormApp.ItemType.CHECKBOX){
-      checkAnswers_(eachResponse.getResponse(),theQuestion.asCheckboxItem())
+    if (theQuestion.getType() == FormApp.ItemType.MULTIPLE_CHOICE) {
+      checkAnswers_([eachResponse.getResponse()], theQuestion.asMultipleChoiceItem())
+    } else if (theQuestion.getType() == FormApp.ItemType.CHECKBOX) {
+      checkAnswers_(eachResponse.getResponse(), theQuestion.asCheckboxItem())
     }
   }
 }
@@ -47,26 +44,24 @@ function reviewResponse_(fullResponse){
 /**
  * Test CHECKBOX and Muliptple choice items and adds if not found in responses.
  */
-function checkAnswers_(theResponse,aQuestion){
-    
-  if(aQuestion.hasOtherOption()){
+function checkAnswers_(theResponse, aQuestion) {
+  if (aQuestion.hasOtherOption()) {
     // var theChoices = getTextChoices_(aQuestion.getChoices());
     var theChoices = aQuestion.getChoices().map(getTextChoices_);
-    
-    for(var i=0;i<theResponse.length;i++){
+    for (var i = 0; i < theResponse.length; i++) {
       var eachResponse = theResponse[i];
-      if(!theChoices.includes(eachResponse)){
+      if (!theChoices.includes(eachResponse)) {
         theChoices.push(eachResponse);
+        theChoices.sort();
         aQuestion.setChoiceValues(theChoices);
       }
     }
   }
 }
-     
+
 /**
  * Turns choices into text
- */ 
-function getTextChoices_(theItem){
-
+ */
+function getTextChoices_(theItem) {
   return theItem.getValue();
 }
