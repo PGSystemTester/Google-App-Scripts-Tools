42387feb9ca27403aed2f726ca5111d553be6f0d
pgSystemTester <7731989+PGSystemTester@users.noreply.github.com>
2022-04-10 13:11:36 -0700

    Create New Sheets Function for TextSplit

---
+++ sheets/textSplit.js
@@ -0,0 +1,48 @@
+/*
+* @OnlyCurrentDoc
+*/
+function zTextSplit(theText, col_delimeter, row_delimiter, ignore_empty, pad_with) {
+  var allLines = [];
+
+  if (row_delimiter === ''|| row_delimiter===undefined) {
+    allLines = theText.split(col_delimeter);
+    if(ignore_empty){
+      allLines = allLines.filter(x => x != '');
+    }
+    return [allLines];
+  } else {
+    var oneLine = [];
+    ;
+    var zIndex = 0;
+    if (row_delimiter != "") {
+      var endCharcter = row_delimiter;
+    } else {
+      endCharcter = col_delimeter;
+    }
+
+    if (theText.slice(theText.length - 1, theText.length) != endCharcter) {
+      theText = theText + endCharcter;
+    }
+
+    for (var i = 0; i < theText.length; i++) {
+      var zLetter = theText.slice(i, i + 1);
+      if (zLetter == col_delimeter || zLetter == row_delimiter) {
+        var theCell = theText.slice(zIndex, i);
+        zIndex = i + 1;
+        oneLine.push(theCell)
+
+        if (zLetter == row_delimiter) {
+          if (ignore_empty) {
+            oneLine = oneLine.filter(x => x != '');
+          }
+          if (oneLine.length > 0 || !ignore_empty) {
+            allLines.push(oneLine);
+          }
+          oneLine = [];
+        }
+      }
+    }
+    return allLines;
+  }
+
+}
