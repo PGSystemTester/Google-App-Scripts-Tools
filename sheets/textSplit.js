116b9836b63f050eb83e401a45d5a69e2ee37bc7
pgSystemTester <7731989+PGSystemTester@users.noreply.github.com>
2022-04-10 13:25:03 -0700

    Updated Descriptions

--- sheets/textSplit.js
+++ sheets/textSplit.js
@@ -1,6 +1,18 @@
 /*
 * @OnlyCurrentDoc
 */
+
+/**
+ * Splits cell values similar to text to column. Built after MS Announced new functions 2022.
+ *
+ * @param {string} theText the text to search
+ * @param {string} col_delimeter The Column Splitter
+ * @param {[string]} row_delimiter Optional row spliter
+ * @param {[boolean]} ignore_empty skip empty values.
+ * @param {[string]} pad_with not seeup yet
+ * @return An array of new text values.
+ * @customfunction
+ */
 function zTextSplit(theText, col_delimeter, row_delimiter, ignore_empty, pad_with) {
   var allLines = [];
 
